cc34b8245d94048d25ba37e958ee8426ee9ecce1
Juampi <juanpablo.cantuna@gmail.com>
2021-07-04 20:50:05 +0200

    Created basic components

---
+++ newsweb/src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Nav from '../Nav/Nav';
+import './Header.css';
+
+class Header extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Cabecera</h1>
+            </div>
+        );
+    }
+}
+
+export default Header;
